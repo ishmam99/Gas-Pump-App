0400ef4c3471d449f36a946e116e4d11d054d0e3
Your Name <you@example.com>
2021-12-02 02:35:01 +0600

    update

--- resources/views/product/sale.blade.php
+++ resources/views/product/sale.blade.php
@@ -4,13 +4,14 @@
       
       <div class="sl-pagebody">
         <div class="sl-page-title">
-          <h5>Form Layouts</h5>
-          <p>Forms are used to collect user information with different element types of input, select, checkboxes, radios and more.</p>
+          <h5>POS</h5>
+          
         </div><!-- sl-page-title -->
 
         <div class="card pd-50 pd-sm-70">
-          <h6 class="card-body-title">Top Label Layout</h6>
-          <p class="mg-b-20 mg-sm-b-30">A form with a label on top of each form control.</p>
+         
+          <h6 class="card-body-title">Make Sale <a href="#" class="btn btn-sm btn-warning" style="float: right;" data-toggle="modal" data-target="#modaldemo3">Add New Vehicle</a></h6>
+          <p class="mg-b-20 mg-sm-b-30">Select a Company name and available vehicles will be loaded automatically.</p>
 
           <div class="form-layout">
             <div class="row mg-b-25">
@@ -97,7 +98,70 @@
           </div><!-- form-layout -->
         </div><!-- card -->
         </div>
+
       </div>
+<!--- Modal -->
+<div id="modaldemo3" class="modal fade">
+          <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content tx-size-sm">
+              <div class="modal-header pd-x-20">
+                <h3 class="tx-15 mg-b-10 tx-uppercase tx-inverse tx-bold">Add new Vehicle</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div> 
+              @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{$error}}</li>
+                      @endforeach
+                    </ul>
+                </div>
+                  
+              @endif
+              
+              <form method="POST" action="{{route('store.vehicle')}}">
+                @csrf
+              <div class="modal-body pd-20 mg-20">
+               <div class="form-group">
+                    <label for="category">Vehicle Number</label>
+                    <input type="text" class="form-control" placeholder="Vehcile Number" name="vehicle_number">
+
+                  </div>
+                  <div class="form-group">
+                    <label for="category">Company Name</label>
+                    <select class="form-control select2" data-placeholder="Choose Category" name="company_id">
+                    @foreach ($company as $item)
+                        <option value="{{$item->id}}">{{$item->name}}</option>
+                    @endforeach
+                   
+                    
+                  </select>
+
+                  </div>
+                  <div class="form-group">
+                    <label for="category">Owener Name</label>
+                    <input type="text" class="form-control" placeholder="Category Name" name="owener_name">
+
+                  </div>
+                  <div class="form-group">
+                    <label for="category">Owener Phone</label>
+                    <input type="text" class="form-control" placeholder="Phone" name="owener_phone">
+
+                  </div>
+           
+              </div><!-- modal-body -->
+              <div class="modal-footer">
+                <button type="submit" class="btn btn-info pd-x-20">Submit</button>
+                <button type="button" class="btn btn-secondary pd-x-20" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+                 </form>
+          </div><!-- modal-dialog -->
+        </div><!-- modal -->
+
+<!--End Modal -->
 
 
    
--- resources/views/vehicle/index.blade.php
+++ resources/views/vehicle/index.blade.php
@@ -127,23 +127,6 @@
         </div><!-- modal -->
 
 
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <h3 >Add new Vehicle sa  d   g  j d a e a e w Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</h3>
-                
-      </div>
-      
-    </div>
-  </div>
-</div>
        
 
 @endsection
